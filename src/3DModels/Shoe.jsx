46edbc2c970bfcd82fdc47770473678fc62fd9cd
MSiesto <miguelsiesto@gmail.com>
2024-02-05 16:04:32 +0100

    adding rest of files

---
+++ src/3DModels/Shoe.jsx
@@ -0,0 +1,52 @@
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+function Shoe({ customColors, ...props }) {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/shoe.gltf");
+  return (
+    <group ref={group} {...props} dispose={null} scale={3}>
+      <mesh
+        geometry={nodes.shoe.geometry}
+        material={materials.laces}
+        material-color={customColors.soul}
+      />
+      <mesh
+        geometry={nodes.shoe_1.geometry}
+        material={materials.mesh}
+        material-color={customColors.mesh}
+      />
+      <mesh
+        geometry={nodes.shoe_2.geometry}
+        material={materials.caps}
+        material-color={customColors.soul}
+      />
+      <mesh
+        geometry={nodes.shoe_3.geometry}
+        material={materials.inner}
+        material-color={customColors.soul}
+      />
+      <mesh
+        geometry={nodes.shoe_4.geometry}
+        material={materials.sole}
+        material-color={customColors.soul}
+      />
+      <mesh
+        geometry={nodes.shoe_5.geometry}
+        material={materials.stripes}
+        material-color={customColors.stripes}
+      />
+      <mesh
+        geometry={nodes.shoe_6.geometry}
+        material={materials.band}
+        material-color={customColors.soul}
+      />
+      <mesh
+        geometry={nodes.shoe_7.geometry}
+        material={materials.patch}
+        material-color={customColors.soul}
+      />
+    </group>
+  );
+}
+export { Shoe };
---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useState, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei"; //esto es lo que nos permite rotar cosas, uno de los formatos que maneja THREE es GLTF , este es el hoook que usaremos para cargar los modulos 3d
+import { Shoe } from "./3DModels/Shoe";
+import "./App.css";
+
+function App() {
+  const [mesh, setMesh] = useState("#ffffff");
+  const [stripes, setStripes] = useState("#ffffff");
+  const [soul, setSoul] = useState("#ffffff");
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="card">
+          <div className="product-canvas">
+            <Canvas>
+              <Suspense fallback={null}>
+                <ambientLight />
+                <spotLight
+                  intensity={1000}
+                  angle={2}
+                  penumbra={1}
+                  position={[10, 15, 10]}
+                  castShadow
+                />
+                <Shoe
+                  customColors={{
+                    mesh: mesh,
+                    stripes: stripes,
+                    soul: soul,
+                  }}
+                />
+                <OrbitControls
+                  enablePan={true}
+                  enableZoom={true}
+                  enableRotate={true}
+                />
+              </Suspense>
+            </Canvas>
+          </div>
+          <h2>Color chooser</h2>
+          <div className="colors">
+            <div>
+              <input
+                type="color"
+                id="head"
+                name="head"
+                value={mesh}
+                onChange={(e) => setMesh(e.target.value)}
+              />
+              <label for="head">Main</label>
+            </div>
+
+            <div>
+              <input
+                type="color"
+                id="body"
+                name="body"
+                value={stripes}
+                onChange={(e) => setStripes(e.target.value)}
+              />
+              <label for="body">Stripes</label>
+            </div>
+            <div>
+              <input
+                type="color"
+                id="body"
+                name="body"
+                value={soul}
+                onChange={(e) => setSoul(e.target.value)}
+              />
+              <label for="body">Soul</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
